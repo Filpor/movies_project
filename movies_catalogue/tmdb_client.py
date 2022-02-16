6688ab8579cad0c3ebfd3111270af3d63b020277
Filpor <filip.porebinski@o2.pl>
2022-02-16 15:39:22 +0100

    Dodanie szczegółów filmu i innych list

--- movies_catalogue/tmdb_client.py
+++ movies_catalogue/tmdb_client.py
@@ -1,20 +1,44 @@
 import requests
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzM2ZDFmNmRlMGJiMmU5Njk3ZWY2OGZjMTE3MGJmMSIsInN1YiI6IjYyMGFhMjNlZWY4YjMyMDAxYjY5ZmRiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bZFR-2sRj8ZrGxIQyZIqFTi8ESjxLQPYTvLb0CqfAUU"
+API_TOKEN='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzM2ZDFmNmRlMGJiMmU5Njk3ZWY2OGZjMTE3MGJmMSIsInN1YiI6IjYyMGFhMjNlZWY4YjMyMDAxYjY5ZmRiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bZFR-2sRj8ZrGxIQyZIqFTi8ESjxLQPYTvLb0CqfAUU'
+
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
-    print(response)
+    response.raise_for_status()
     return response.json()
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
     return data["results"][:how_many]
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()['cast']
+
+def get_cast(movie_id,how_many):
+    data=get_single_movie_cast(movie_id)
+    return data['cast'][:how_many]
+
+
+
 
