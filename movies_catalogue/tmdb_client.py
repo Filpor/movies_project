992ab1ae7ac8a708b35398c8839a3b686ee15f46
Filpor <filip.porebinski@o2.pl>
2022-03-01 19:30:31 +0100

    test_homepage

--- movies_catalogue/tmdb_client.py
+++ movies_catalogue/tmdb_client.py
@@ -2,14 +2,17 @@ import requests
 
 API_TOKEN='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NzM2ZDFmNmRlMGJiMmU5Njk3ZWY2OGZjMTE3MGJmMSIsInN1YiI6IjYyMGFhMjNlZWY4YjMyMDAxYjY5ZmRiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bZFR-2sRj8ZrGxIQyZIqFTi8ESjxLQPYTvLb0CqfAUU'
 
-def get_movies_list(list_type):
-    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
+def call_tmdb_api(endpoint):
+    full_url=f'https://api.themoviedb.org/3/{endpoint}'
+    headers={
+        'Authorization':f'Bearer {API_TOKEN}'
     }
-    response = requests.get(endpoint, headers=headers)
+    response =requests.get(full_url, headers=headers)
     response.raise_for_status()
     return response.json()
+
+def get_movies_list(list_type):
+    return call_tmdb_api(f'movie/{list_type}')
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
@@ -20,20 +23,10 @@ def get_movies(how_many, list_type):
     return data["results"][:how_many]
 
 def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f'movie/{movie_id}')
 
 def get_single_movie_cast(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f'movie/{movie_id}/credits')
 
 def get_cast(movie_id,how_many):
     data=get_single_movie_cast(movie_id)
